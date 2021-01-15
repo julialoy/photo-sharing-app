5c233cf30f884d75129bf78f9e53fefbffc1c53a
Julia Loy <julialoy@gmail.com>
2021-01-15 17:30:46 -0500

    Fixed thumb/web orientation bug.

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 # from email.message import EmailMessage
+import functools
 import json
 import os
 import random
@@ -109,6 +110,44 @@ def parse_image_data(db_row):
 def order_images(parsed_image_array):
     """Sort image array so most recent appears first."""
     parsed_image_array.sort(key=lambda x: x['date_taken'], reverse=True)
+
+
+def fix_image_orientation(im):
+    """
+    Taken from Stack Overflow answer for exif orientation.
+    -----------
+    Apply Image.transpose to ensure 0th row of pixels is at the visual
+    top of the image, and 0th column is the visual left-hand side.
+    Return the original image if unable to determine the orientation.
+
+    As per CIPA DC-008-2012, the orientation field contains an integer,
+    1 through 8. Other values are reserved.
+
+    Parameters
+    ----------
+    im: PIL.Image
+       The image to be rotated.
+    """
+
+    exif_orientation_tag = 0x0112
+    exif_transpose_sequences = [                   # Val  0th row  0th col
+        [],                                        #  0    (reserved)
+        [],                                        #  1   top      left
+        [Image.FLIP_LEFT_RIGHT],                   #  2   top      right
+        [Image.ROTATE_180],                        #  3   bottom   right
+        [Image.FLIP_TOP_BOTTOM],                   #  4   bottom   left
+        [Image.FLIP_LEFT_RIGHT, Image.ROTATE_90],  #  5   left     top
+        [Image.ROTATE_270],                        #  6   right    top
+        [Image.FLIP_TOP_BOTTOM, Image.ROTATE_90],  #  7   right    bottom
+        [Image.ROTATE_90],                         #  8   left     bottom
+    ]
+
+    try:
+        seq = exif_transpose_sequences[im._getexif()[exif_orientation_tag]]
+    except Exception:
+        return im
+    else:
+        return functools.reduce(type(im).transpose, seq, im)
 
 
 def generate_invite_code():
@@ -580,6 +619,7 @@ async def upload_handler(request: web.Request) -> web.json_response:
                     img = Image.open(image_file)
                     exif_data = img.getexif()
                     creation_exif_date = exif_data.get(36867)
+                    img = fix_image_orientation(img)
 
                     if creation_exif_date is None:
                         creation_date = datetime.datetime.today()
